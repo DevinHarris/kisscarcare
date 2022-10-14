5a2b7c1b6d5b8e1edc7a42c6ee1c556a1ab27976
Devonta Harris <devontah94@gmail.com>
2022-10-14 01:29:56 -0400

    finish landing page build

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import Layout from '../components/Layout'
 // import styles from '../styles/Home.module.css'
 import styles from '../styles/Landing.module.css';
@@ -8,28 +9,28 @@ import bgImg from '../public/img/bg-img-5.jpg'
 export default function Home() {
   return (
     <>
-      <Layout title="Landing.">
+      <Layout title="Kiss Car Care - Keep car detailing simple.">
         <div className={styles.landingContainer}>
           <div className={styles.landingContent}>
             <div className={styles.landingMain}>
               <h1 className={styles.landingTagline}>Keep car detailing simple.</h1>
               <p className={styles.landingText}>Book one of our trained technicians and we'll come to you.</p>
             </div>
-            <div className={styles.landingFooter}>
+            <footer className={styles.landingFooter}>
               <nav className={styles.landingNav}>
                 <ul>
-                  <li>
-                    <a href="#">Book an appointment</a>
+                  <li className={styles.linkActive}>
+                    <Link href="/quote">Get a quote</Link>
                   </li>
-                  <li>
-                    <a href="#">Meet our technicians</a>
+                  <li className={styles.footerLink}>
+                    <Link href="/team">Meet our technicians</Link>
                   </li>
-                  <li>
-                    <a href="#">View our services</a>
+                  <li className={styles.footerLink}>
+                    <Link href="/services">View our services</Link>
                   </li>
                 </ul>
               </nav>
-            </div>
+            </footer>
           </div>
         </div>
       </Layout>
---
+++ pages/quote/index.js
@@ -0,0 +1,5 @@
+export default function QuotePage() {
+    return (
+        <div>Quote page</div>
+    )
+}
